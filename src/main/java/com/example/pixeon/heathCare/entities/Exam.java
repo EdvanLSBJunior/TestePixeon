26513a0bb94cbdb2d7d70c4a0f1f6c86615610db
Edvan Junior <edvanbrito.j@hotmail.com>
2022-07-29 16:53:20 -0300

    removed methods set entity id

--- src/main/java/com/example/pixeon/heathCare/entities/Exam.java
+++ src/main/java/com/example/pixeon/heathCare/entities/Exam.java
@@ -29,10 +29,6 @@ public class Exam {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPatientName() {
         return patientName;
     }
--- src/main/java/com/example/pixeon/heathCare/entities/HealthCareInstitution.java
+++ src/main/java/com/example/pixeon/heathCare/entities/HealthCareInstitution.java
@@ -24,10 +24,6 @@ public class HealthCareInstitution implements Serializable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
